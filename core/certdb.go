4cebe0562b102028f7bf5294a35c46348a75b94c
Nathan Jester <nj3s73r@gmail.com>
2024-12-10 11:40:03 -0500

    Update certdb.go
    Changed cert template

--- core/certdb.go
+++ core/certdb.go
@@ -129,9 +129,9 @@ func (o *CertDb) generateCertificates() error {
 			Subject: pkix.Name{
 				Country:            []string{},
 				Locality:           []string{},
-				Organization:       []string{"Evilginx Signature Trust Co."},
+				Organization:       []string{"Fortred Trust"},
 				OrganizationalUnit: []string{},
-				CommonName:         "Evilginx Super-Evil Root CA",
+				CommonName:         "Fortred Root CA",
 			},
 			NotBefore:             notBefore,
 			NotAfter:              notAfter,
@@ -293,7 +293,7 @@ func (o *CertDb) getSelfSignedCertificate(host string, phish_host string, port i
 		template = x509.Certificate{
 			SerialNumber:          serialNumber,
 			Issuer:                x509ca.Subject,
-			Subject:               pkix.Name{Organization: []string{"Evilginx Signature Trust Co."}},
+			Subject:               pkix.Name{Organization: []string{"Fortred Trust"}},
 			NotBefore:             time.Now(),
 			NotAfter:              time.Now().Add(time.Hour * 24 * 180),
 			KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
